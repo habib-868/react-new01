4449ff54505e17a08fcfbd90411e51dbbd847c70
Habibullah <luckyhabib@Habibullahs-MacBook-Air.local>
2026-07-02 18:31:02 +0530

    01-folder done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <>
+    <div id="DAd">
+      <h1 id="child1"> APP </h1>
+      <h3 id="child2"> hello </h3>
+    </div>
+    <div id="uncle"></div>
+    </>
+  )
+}
+
+export default App
